0ff968bbe14519bd7266f36e09cfb750fd13ec5f
dhorki <dhorki@gmail.com>
2022-01-27 15:10:26 -0600

    Added favicon Added url validation to sprites urls

--- pokemon_app/app/models/sprite.rb
+++ pokemon_app/app/models/sprite.rb
@@ -18,14 +18,16 @@ class Sprite < ApplicationRecord
   # validates_length_of :front_shiny_female, :maximum => 255, :allow_blank => true
   # validates_length_of :official_artwork_front_default, :maximum => 255, :allow_blank => true
 
+  URL_REGEX = %r/.{}|(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
+
   validates :pokemon_id, presence: true, uniqueness: true
-  validates :back_default, length: { maximum: 255, allow_blank: true }
-  validates :back_female, length: { maximum: 255, allow_blank: true }
-  validates :back_shiny, length: { maximum: 255, allow_blank: true }
-  validates :back_shiny_female, length: { maximum: 255, allow_blank: true }
-  validates :front_default, length: { maximum: 255, allow_blank: true }
-  validates :front_female, length: { maximum: 255, allow_blank: true }
-  validates :front_shiny, length: { maximum: 255, allow_blank: true }
-  validates :front_shiny_female, length: { maximum: 255, allow_blank: true }
-  validates :official_artwork_front_default, length: { maximum: 255, allow_blank: true }
+  validates :back_default, length: { maximum: 255, allow_blank: true }, format: { with: URL_REGEX, allow_blank: true }
+  validates :back_female, length: { maximum: 255, allow_blank: true }, format: {  with: URL_REGEX, allow_blank: true }
+  validates :back_shiny, length: { maximum: 255, allow_blank: true }, format: { with: URL_REGEX, allow_blank: true }
+  validates :back_shiny_female, length: { maximum: 255, allow_blank: true }, format: { with: URL_REGEX, allow_blank: true }
+  validates :front_default, length: { maximum: 255, allow_blank: true }, format: { with: URL_REGEX, allow_blank: true }
+  validates :front_female, length: { maximum: 255, allow_blank: true }, format: { with: URL_REGEX, allow_blank: true }
+  validates :front_shiny, length: { maximum: 255, allow_blank: true }, format: { with: URL_REGEX, allow_blank: true }
+  validates :front_shiny_female, length: { maximum: 255, allow_blank: true }, format: { with: URL_REGEX, allow_blank: true }
+  validates :official_artwork_front_default, length: { maximum: 255, allow_blank: true }, format: { with: URL_REGEX, allow_blank: true }
 end
